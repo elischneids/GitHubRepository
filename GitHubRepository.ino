f44f8456baea59a36a91c88a26b142834be200a0
elischneids <e.schneider1210@gmail.com>
2025-11-04 13:21:23 -0500

    button turns on leds

--- GitHubRepository.ino
+++ GitHubRepository.ino
@@ -7,16 +7,16 @@ void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(ledPin2, OUTPUT);
   pinMode(buttonPin, INPUT);
-
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH):
-  digitalWrite(ledPin2, HIGH):
-  delay(500);
-  digitalWrite(ledPin, LOW):
-  digitalWrite(ledPin2, LOW):
-  delay(500);
-
-
+  if (analogRead(buttonPin) == HIGH){
+    digitalWrite(ledPin, HIGH):
+    digitalWrite(ledPin2, HIGH):
+    delay(500);
+  } else {
+    digitalWrite(ledPin, LOW):
+    digitalWrite(ledPin2, LOW):
+    delay(500);
+  }
 }
